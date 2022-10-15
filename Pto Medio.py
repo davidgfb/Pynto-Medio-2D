fa1d316fcb36b3d65b5c6e14f684275091afabb5
davidgfb <davidgracia99@gmail.com>
2022-10-15 18:30:41 +0200

    Update Pto Medio.py

--- Pto Medio.py
+++ Pto Medio.py
@@ -2,29 +2,9 @@ from tkinter import Tk, Entry
 from numpy import array, isin
 
 malla, n_Filas, n_Columnas, ancho, root = [], 5, 5, 10, Tk()
-ns_Filas, ns_Columnas = range(n_Filas), range(n_Columnas)
+ns_Filas, ns_Columnas = range(n_Filas), range(n_Columnas)   
 
-def dibuja_Ptos_Medios_Calculados(ps):
-    p_0, p_F = ps
-       
-    if (p_0 == p_F).all():
-        p_0_X, p_0_Y = p_0
-
-        malla[p_0_X][p_0_Y].insert(0, 1)
-        
-    else: 
-        ptos_Linea = [p_0, p_F]
-
-        ptoMedio = (p_0 + p_F) // 2
-
-        if not isin(ptoMedio, ptos_Linea).all():
-            ptos_Linea.append(ptoMedio)
-
-        for pto_Linea in ptos_Linea:
-            pto_Linea_X, pto_Linea_Y = pto_Linea
-            
-            malla[pto_Linea_X][pto_Linea_Y].insert(0, 1)
-        
+#PROBADOR
 for n_Fila in ns_Filas:
     fila = []
     
@@ -37,14 +17,37 @@ for n_Fila in ns_Filas:
 
 malla = array(malla[::-1]).transpose()
 
-#PROBADOR
+ps = array((0, 0)), array((4, 4))
+p_0, p_F = ps
+   
+if (p_0 == p_F).all():
+    p_0_X, p_0_Y = p_0
+
+    malla[p_0_X][p_0_Y].insert(0, 1)
+    
+else:
+    ptos_Linea = [p_0, p_F]
+    i = 0
+    lon_Ptos_Linea = len(ptos_Linea)
+
+    while i < lon_Ptos_Linea:
+        p_0, p_F = ptos_Linea[-2::]
+        ptoMedio = (p_0 + p_F) // 2
+
+        if not isin(ptoMedio, ptos_Linea).all():
+            ptos_Linea.append(ptoMedio)
+
+        lon_Ptos_Linea = len(ptos_Linea)
+        i += 1
+    
+    for pto_Linea in ptos_Linea:
+        pto_Linea_X, pto_Linea_Y = pto_Linea
+        
+        malla[pto_Linea_X][pto_Linea_Y].insert(0, 1)
+
 '''for n_Fila in ns_Filas:
     for n_Columna in ns_Columnas:
         malla[n_Fila][n_Columna].insert(0, array((n_Fila, n_Columna)))'''
-
-ps = array((0, 0)), array((4, 4))
-p_0, p_F = ps
-dibuja_Ptos_Medios_Calculados(ps)
 
 '''ps = array((0, 0)), array((0, 0))
 p_0, p_F = ps                              
